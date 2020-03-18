18e5ef4d10bc4f5b91a2ba87d13f047d7b5ba33a
fbuitron <franciscobuitron21@gmail.com>
2020-03-18 15:20:06 -0700

    initial commit and added form in card

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Employees from './components/Employees'
+import Form from './components/Form'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Form />
+      </>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Form/Form.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Form.css'
+
+const Form = props => {
+  return (
+    <div className='card'>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
+        
+export default Form
